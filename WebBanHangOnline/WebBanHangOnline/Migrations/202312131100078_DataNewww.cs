f8fe973ef6e5f44f73a5bdc56866bcdc601c4bf9
huunam <huunam123>
2023-12-15 16:56:23 +0700

    First commit

---
+++ WebBanHangOnline/WebBanHangOnline/Migrations/202312131100078_DataNewww.cs
@@ -0,0 +1,22 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataNewww : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Category", "Link", c => c.String());
+            AddColumn("dbo.tb_Order", "Status", c => c.Int(nullable: false));
+            AddColumn("dbo.tb_Product", "OriginalPrice", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Product", "OriginalPrice");
+            DropColumn("dbo.tb_Order", "Status");
+            DropColumn("dbo.tb_Category", "Link");
+        }
+    }
+}
